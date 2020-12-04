a9a458b6abfff670cd54bff00837e66554083fe1
Devin Singh <drsingh2518@icloud.com>
2020-12-04 16:25:08 -0600

    day01 algo optimization to n and n^2

--- src/day01.rs
+++ src/day01.rs
@@ -1,37 +1,37 @@
 use std::fs;
 
 pub fn sum_to_year() -> u32 {
+    // Stupid way to push each line into a vec
     let file = fs::read_to_string("input_day01.txt").unwrap();
     let mut vec: Vec<u32> = Vec::new();
     for fileline in file.lines() {
         vec.push(fileline.parse::<u32>().unwrap());
     }
 
-    for (idx, i) in vec.iter().enumerate() {
-        for j in vec[idx + 1..].iter() {
-            if i + j == 2020 {
-                println!("{} {}, {}", i, j, i * j);
-                return i * j;
-            }
+    for i in &vec {
+        if vec.contains(&(2020 - i)) {
+            println!("{} {}, {}", i, 2020 - i, (2020 - i) * i);
+            return i * (2020 - i);
         }
     }
+
     0
 }
 
 pub fn sum_of_three() -> u32 {
+    // Stupid way to push each line into a vec
     let file = fs::read_to_string("input_day01.txt").unwrap();
-    let mut vec: Vec<u32> = Vec::new();
+    let mut vec: Vec<i32> = Vec::new();
     for fileline in file.lines() {
-        vec.push(fileline.parse::<u32>().unwrap());
+        vec.push(fileline.parse::<i32>().unwrap());
     }
 
-    for (idx, i) in vec.iter().enumerate() {
-        for j in vec[idx + 1..].iter() {
-            for k in vec[idx + 2..].iter() {
-                if i + j + k == 2020 {
-                    println!("{} {} {}, {}", i, j, k, i * k * j);
-                    return i * k * j;
-                }
+    'outer: for i in &vec {
+        for j in &vec {
+            let leftover = 2020 - i - j;
+            if vec.contains(&leftover) {
+                println!("{} {} {}, {}", i, j, leftover, leftover * i * j);
+                break 'outer;
             }
         }
     }
