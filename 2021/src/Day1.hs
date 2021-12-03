81a3d6f7923299a4ed2267c2edd806971649f16a
Devin Singh <drsingh2518@icloud.com>
2021-12-02 18:05:36 -0600

    2021 day1 fix

--- 2021/src/Day1.hs
+++ 2021/src/Day1.hs
@@ -3,8 +3,9 @@ module Day1 ( day1Main ) where
 day1Main :: IO ()
 day1Main = do
     content <- readFile "input/day1.txt"
-    print $ countIncreases (map read ( lines content ))
-    print $ countSlidingIncreases (map read ( lines content ))
+    putStrLn "Day1:"
+    print $ countIncreases (map read (lines content))
+    print $ countSlidingIncreases (map read (lines content))
 
 countIncreases :: [Int] -> Int
 countIncreases [] = 0
@@ -12,10 +13,10 @@ countIncreases [_] = 0
 countIncreases (x:xs) = fromEnum ( x < head xs ) + countIncreases xs
 
 countSlidingIncreases :: [Int] -> Int
-countSlidingIncreases xs = sum (windowOfThree xs) + sum (countSlidingIncreases (tail xs))
+countSlidingIncreases [] = 0
+countSlidingIncreases xs = fromEnum (sumOfThree xs < sumOfThree (tail xs)) + countSlidingIncreases (tail xs)
 
-windowOfThree :: [Int] -> [Int]
-windowOfThree [] = []
-windowOfThree [x] = [x, 0, 0]
-windowOfThree [x, y] = [x, y, 0]
-windowOfThree (x:xs) = [x, head xs, head $ tail xs]
+sumOfThree :: [Int] -> Int
+sumOfThree [x] = x
+sumOfThree [x, y] = x + y
+sumOfThree xs = sum (take 3 xs)
--- 2021/src/Lib.hs
+++
@@ -1,5 +0,0 @@
-module Lib
-    ( module Day1
-    ) where
-
-import qualified Day1
